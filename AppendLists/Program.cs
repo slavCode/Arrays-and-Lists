ca691d60a65722c4e184beac79309c9d2a74bcd8
slavCode <stomow@gmail.com>
2022-04-04 14:24:27 +0300

    Done Couple of Exercises for Lists

---
+++ AppendLists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLists
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            var tokens = input.Split('|');
+            var result = new List<string>();
+            for (int i = tokens.Length - 1; i >= 0; i--)
+            {
+                var token = tokens[i].Trim().Split();
+                for (int j = 0; j < token.Length; j++)
+                {
+                    if (token[j] != "") result.Add(token[j]);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
---
+++ RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var result = new List<int>();
+
+            foreach (var item in numbers)
+            {
+                if (item > 0)
+                {
+                    result.Add(item);
+                }
+            }
+
+            if (result.Count > 0)
+            {
+                result.Reverse();
+                Console.WriteLine(String.Join(" ", result));
+            }
+            else Console.WriteLine("empty");
+        }
+    }
+}
