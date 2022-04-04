912deeaac0b2c6de0c7915260c03ff061a9569c3
slavCode <stomow@gmail.com>
2022-04-04 12:59:11 +0300

    Done with Array Exercises in the lab

---
+++ ExtractMiddle)neTwoOrThreeElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractMiddle_neTwoOrThreeElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int counter = numbers.Length;
+
+            if (counter == 1)
+            {
+                Console.WriteLine($"{{ {numbers[0]} }}");
+                return;
+            }
+            if (counter % 2 == 0)
+            {
+                Console.WriteLine($"{{ {numbers[counter / 2 - 1]}, {numbers[(counter / 2)]} }}");
+            }
+            else
+            {
+                var index = (int)Math.Ceiling(counter / 2m);
+                var prevIndex = index - 1;
+                var nextIndex = index + 1;
+                Console.WriteLine($"{{ {prevIndex}, {index}, {nextIndex} }}");
+            }
+        }
+    }
+}
